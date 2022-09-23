461ea6d9bc77d725ad92b09ea6f740e43c3e32ae
IvanSisa <navisisaez@gmail.com>
2022-09-24 00:55:18 +0200

    Up Fixcar-Back

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { mongoConnect } from './services/db.js';
+import morgan from 'morgan';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import garagesRouter from './routes/garages.routes.js';
+import loginRouter from './routes/login.routes.js';
+dotenv.config();
+mongoConnect();
+export const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/garages', garagesRouter);
+app.use('/login', loginRouter);
+
+app.use((err, req, resp) => {
+    console.log(err.message);
+    resp.send({ error: err.message });
+});
+
+app.post('/garages', (req, res) => {
+    console.log('nuevo endpoint');
+
+    res.send('He usado POST');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+export const server = app.listen(port, () => {
+    console.log(`Server esta escuchando en http://localhost:${port} `);
+});
---
+++ models/garage.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+export function garageCreator(modelName = 'Garage') {
+    const garageSchema = new mongoose.Schema({
+        user: { type: String, required: true, unique: true },
+        pass: { type: String, required: true },
+        garage_name: { type: String, required: true },
+        cif_nif: { type: String, required: true },
+        email: { type: String, required: true },
+        phone: { type: String, required: true },
+        web: { type: String, required: true },
+        address: { type: String, required: true },
+        services: {
+            ruedas: Number,
+            aceite: Number,
+            filtros: Number,
+            amortiguadores: Number,
+            discos: Number,
+            pastillas: Number,
+            aire: Number,
+            bombillas: Number,
+        },
+    });
+
+    garageSchema.set('toJSON', {
+        transform: (document, returnedObject) => {
+            delete returnedObject.__v;
+        },
+    });
+
+    let Garage;
+    if (mongoose.default.models[modelName]) {
+        Garage = mongoose.model(modelName);
+    } else {
+        Garage = mongoose.model(modelName, garageSchema);
+    }
+    return Garage;
+}
+
+export const Garage = garageCreator();
